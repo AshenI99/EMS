4a93aa742dc4a69c9a250d170ce2216a01ec6121
AshenI99 <asheniranda99@gmail.com>
2022-11-12 11:04:22 +0530

    Test

--- startup/config.js
+++ startup/config.js
@@ -1,6 +1,7 @@
 const config = require("config");
 
 module.exports = function () {
+    console.log(config)
     if(!config.get("jwtPrivateKey")){
         throw new Error("Fatal Error: jwtPrivateKey is not defined.");
     }
